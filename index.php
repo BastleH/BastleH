6858a4361adf3a6ab1f50c20c17f2db976352efe
BastleH <thomas.henry100@hotmail.com>
2020-08-23 18:59:55 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<? php 
+
+/* TODO lembrete para revisar cálculos */
+/* FIXME corrigir alguma coisa no futuro */
+
+/** @author Thomas <thomas.henry100@hotail.com> */
+phpinfo();
+phpinfo();
+
+/**
+ * Class constructor.
+ */
+
